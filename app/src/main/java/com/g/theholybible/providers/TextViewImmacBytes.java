76497f8a51343fb45741db4e8e76a7ff80a183d0
Tania Joseph <taniajoseph37@gmail.com>
2019-10-26 23:08:51 +0530

    Text Changes

---
+++ app/src/main/java/com/g/theholybible/providers/TextViewImmacBytes.java
@@ -0,0 +1,33 @@
+package com.g.theholybible.providers;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+
+public class TextViewImmacBytes extends AppCompatTextView {
+
+    public TextViewImmacBytes(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public TextViewImmacBytes(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public TextViewImmacBytes(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/NexaLight.otf");
+            setTypeface(tf);
+        }
+    }
+
+}
